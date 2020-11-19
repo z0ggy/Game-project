14c55d9a5de934988515a206dfbd97f1c234a302
Dominik Zochniak <z0ggy@pop-os.localdomain>
2020-11-19 21:07:28 +0000

    add game character front view

--- gameProject_2b/sketch.js
+++ gameProject_2b/sketch.js
@@ -36,28 +36,48 @@ function setup()
     canyon = {
         x_pos: -100, 
         width: 100
-    }
+    };
     
     collectable = {
         x_pos: 100, 
         y_pos: 100, 
         size: 50
-    }
+    };
     
     cloud = {
         x_pos: 200,
         y_pos: 100,
         scale: 0.7
-    }
+    };
+    
+    mountain = {
+        x_pos: 200,
+        y_pos: 400,
+        scale: 1.0
+    };
+
+    collectable = {
+        x_pos: 400,
+        y_pos: 400,
+        size: 0.7
+    };
 }
 
 function draw()
 {
 	background(100, 155, 255); //fill the sky blue
-
 	noStroke();
 	fill(0, 155, 0);
-	rect(0, floorPos_y, height, width - floorPos_y); //draw some green ground
+    rect(0, floorPos_y, height, width - floorPos_y); //draw some green ground
+    
+//    mountain
+    fill(150);
+    triangle(mountain.x_pos + 50, mountain.y_pos + 32, 
+             mountain.x_pos + 150, mountain.y_pos - 300,
+             mountain.x_pos + 250, mountain.y_pos + 32);
+    triangle(mountain.x_pos + 150, mountain.y_pos + 32, 
+             mountain.x_pos + 150, mountain.y_pos - 250,
+             mountain.x_pos + 350, mountain.y_pos + 32);
     
 //    Tree
     treePos_y = 350;
@@ -85,29 +105,56 @@ function draw()
     fill(100, 155, 255);
     rect(canyon.x_pos + 180, 432, 40 + canyon.width, 144); //blue canyon gap
     
-//    collectable
-    noStroke();
-	fill(255,0,0); //hat of mushroom
-    arc(400, 400, 50, 50, radians(180), radians(0), CHORD);
-    fill(255); // leg of mushroom
-    rect(393,400,15,45);
-    stroke(255); // white dots
-    strokeWeight(5);
-    point(386,392);
-    point(400,385);
-    point(410,390);
-    
 //    clouds
     fill(255);
 	ellipse(cloud.x_pos, 100, 80 * cloud.scale, 60 * cloud.scale);
 	ellipse(cloud.x_pos + 35 * cloud.scale, 100, 65 * cloud.scale, 45 * cloud.scale);
-	ellipse(cloud.x_pos + 70 * cloud.scale, 100, 40 * cloud.scale, 30 * cloud.scale);
+    ellipse(cloud.x_pos + 70 * cloud.scale, 100, 40 * cloud.scale, 30 * cloud.scale);
+    
+    // game character
+    //Head
+    fill(255, 227, 48); 
+    rect(gameChar_x - 15, gameChar_y - 72, 30, 26, 5);
+    rect(gameChar_x - 6, gameChar_y - 78, 12, 10);
+    //Eyes and mouth
+    stroke(60);
+    strokeWeight(3);
+    point(gameChar_x - 6, gameChar_y - 64);
+    point(gameChar_x + 6, gameChar_y - 64);
+    strokeWeight(2);
+    line(gameChar_x - 3, gameChar_y - 54, gameChar_x + 3, gameChar_y - 54);
+    //Body
+    strokeWeight(1);
+    fill(0);
+    rect(gameChar_x - 18, gameChar_y - 46, 37, 27);
+    //Arms
+    fill(200, 100, 100);
+    rect(gameChar_x - 25, gameChar_y - 46, 7 , 37);
+    rect(gameChar_x + 18, gameChar_y - 46, 7 , 37);
+    //Belt
+    fill(98, 74, 46);
+    rect(gameChar_x - 18, gameChar_y - 19, 36, 7);
+    //Legs
+    fill(0, 0, 255);
+    rect(gameChar_x - 18, gameChar_y - 12, 17, 15);
+    rect(gameChar_x + 1, gameChar_y - 12, 17, 15);
 
+    //    collectable
+    noStroke();
+    fill(255,0,0); //hat of mushroom
+    arc(collectable.x_pos, collectable.y_pos, 50 * collectable.size, 50 * collectable.size, radians(180), radians(0), CHORD);
+    fill(255); // leg of mushroom
+    rect(collectable.x_pos - 5, collectable.y_pos, 15 * collectable.size, 45 * collectable.size);
+    stroke(255); // white dots
+    strokeWeight(5 * collectable.size);
+    point(collectable.x_pos - 10 * collectable.size, collectable.y_pos - 5 * collectable.size);
+    point(collectable.x_pos - 1 * collectable.size, collectable.y_pos - 15 * collectable.size);
+    point(collectable.x_pos + 10 * collectable.size, collectable.y_pos - 10 * collectable.size);
 
 }
 
 function mousePressed()
 {
-
-
+gameChar_x = mouseX;
+gameChar_y = mouseY;
 }
