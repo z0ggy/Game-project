13aeda97f90282509cd9dd7207b3ba174947f3af
Dominik Zochniak <z0ggy@pop-os.localdomain>
2020-12-01 17:54:05 +0000

    add character from game project 2

--- gameProject_3a_starter_template/3/sketch.js
+++ gameProject_3a_starter_template/3/sketch.js
@@ -13,6 +13,11 @@ var gameChar_x;
 var gameChar_y;
 var floorPos_y;
 
+var isLeft;
+var isRight;
+var isPlummenting;
+var isFalling;
+
 
 function setup()
 {
@@ -20,6 +25,11 @@ function setup()
 	floorPos_y = height * 3/4;
 	gameChar_x = width/2;
 	gameChar_y = floorPos_y;
+
+	isLeft = false;
+	isRight = false;
+	isPlummeting = false;
+	isFalling = false;
 }
 
 function draw()
@@ -33,9 +43,6 @@ function draw()
 	noStroke();
 	fill(0,155,0);
 	rect(0, floorPos_y, width, height - floorPos_y); //draw some green ground
-
-	//draw the canyon
-
 
 	//the game character
 	if(isLeft && isFalling)
@@ -66,11 +73,38 @@ function draw()
 	else
 	{
 		// add your standing front facing code
+    //Head
+    fill(255, 227, 48); 
+    rect(gameChar_x - 15, gameChar_y - 72, 30, 26, 5);
+    rect(gameChar_x - 6, gameChar_y - 78, 12, 10);
+    //Eyes and mouth
+    stroke(60);
+    strokeWeight(3);
+    point(gameChar_x - 6, gameChar_y - 64);
+    point(gameChar_x + 6, gameChar_y - 64);
+    strokeWeight(2);
+    line(gameChar_x - 3, gameChar_y - 54, gameChar_x + 3, gameChar_y - 54);
+    //Body
+    strokeWeight(1);
+    fill(0);
+    rect(gameChar_x - 18, gameChar_y - 46, 37, 27);
+    //Arms
+    fill(200, 100, 100);
+    rect(gameChar_x - 25, gameChar_y - 46, 7 , 37);
+    rect(gameChar_x + 18, gameChar_y - 46, 7 , 37);
+    //Belt
+    fill(98, 74, 46);
+    rect(gameChar_x - 18, gameChar_y - 19, 36, 7);
+    //Legs
+    fill(0, 0, 255);
+    rect(gameChar_x - 18, gameChar_y - 12, 17, 15);
+    rect(gameChar_x + 1, gameChar_y - 12, 17, 15);
 
 	}
 
 	///////////INTERACTION CODE//////////
 	//Put conditional statements to move the game character below here
+
 
 }
 
@@ -83,13 +117,33 @@ function keyPressed()
 	//open up the console to see how these work
 	console.log("keyPressed: " + key);
 	console.log("keyPressed: " + keyCode);
+
+	if (keyCode == 37)
+	{	anonymous
+		console.log("isLeft value " + isLeft);
+	}
+	else if (keyCode == 39)
+	{	
+		isRight = true;
+		console.log("isRight value " + isRight);
+	}
 }
 
 function keyReleased()
 {
 	// if statements to control the animation of the character when
 	// keys are released.
+	if (keyCode == 37)
+	{	
+		isLeft = false;
+		console.log("isLeft value " + isLeft);
+	}
+	else if (keyCode == 39)
+	{	
+		isRight = false;
+		console.log("isRight value " + isRight);
+	}
 
-	console.log("keyReleased: " + key);
-	console.log("keyReleased: " + keyCode);
+	// console.log("keyReleased: " + key);
+	// console.log("keyReleased: " + keyCode);
 }
