86bb7c93384ef7fb4e33d4c8bef47b9456627e3a
Dominik Zochniak <z0ggy@pop-os.localdomain>
2020-12-01 19:29:09 +0000

    add more character position

--- gameProject_3a_starter_template/3/sketch.js
+++ gameProject_3a_starter_template/3/sketch.js
@@ -48,57 +48,176 @@ function draw()
 	if(isLeft && isFalling)
 	{
 		// add your jumping-left code
+		//Head
+		fill(255, 227, 48);
+		rect(gameChar_x - 15, gameChar_y - 72, 30, 26, 5);
+		rect(gameChar_x - 6, gameChar_y - 78, 12, 10);
+		//Eyes and mouth
+		stroke(60);
+		strokeWeight(3);
+		point(gameChar_x - 6, gameChar_y - 64);
+		strokeWeight(2);
+		line(gameChar_x - 15, gameChar_y - 54, gameChar_x - 10, gameChar_y - 54);
+		//body
+		strokeWeight(1);
+		fill(0);
+		rect(gameChar_x - 15, gameChar_y - 46, 30, 27);
+		//Belt
+		fill(98, 74, 46);
+		rect(gameChar_x - 15, gameChar_y - 19, 30, 7);
+		//Legs
+		fill(0, 0, 255);
+		rect(gameChar_x - 5, gameChar_y - 12, 20, 8);
+		//Arms
+		fill(200, 100, 100);
+		rect(gameChar_x - 7, gameChar_y - 46, 15, 43);
+	
 
 	}
 	else if(isRight && isFalling)
 	{
 		// add your jumping-right code
+		//Head
+		fill(255, 227, 48);
+		rect(gameChar_x - 15, gameChar_y - 72, 30, 26, 5);
+		rect(gameChar_x - 6, gameChar_y - 78, 12, 10);
+		//Eyes and mouth
+		stroke(60);
+		strokeWeight(3);
+		point(gameChar_x + 6, gameChar_y - 64);
+		strokeWeight(2);
+		line(gameChar_x + 10, gameChar_y - 54, gameChar_x +15, gameChar_y - 54);
+		//body
+		strokeWeight(1);
+		fill(0);
+		rect(gameChar_x - 15, gameChar_y - 46, 30, 27);
+		//Belt
+		fill(98, 74, 46);
+		rect(gameChar_x - 15, gameChar_y - 19, 30, 7);
+		//Legs
+		fill(0, 0, 255);
+		rect(gameChar_x - 15, gameChar_y - 12, 20, 8);
+		//Arms
+		fill(200, 100, 100);
+		rect(gameChar_x - 7, gameChar_y - 46, 15, 43);
 
 	}
 	else if(isLeft)
 	{
 		// add your walking left code
+		//Head
+		fill(255, 227, 48);
+		rect(gameChar_x - 15, gameChar_y - 72, 30, 26, 5);
+		rect(gameChar_x - 6, gameChar_y - 78, 12, 10);
+		//Eyes and mouth
+		stroke(60);
+		strokeWeight(3);
+		point(gameChar_x - 6, gameChar_y - 64);
+		strokeWeight(2);
+		line(gameChar_x - 15, gameChar_y - 54, gameChar_x - 10, gameChar_y - 54);
+		//body
+		strokeWeight(1);
+		fill(0);
+		rect(gameChar_x - 15, gameChar_y - 46, 30, 27);
+		//Belt
+		fill(98, 74, 46);
+		rect(gameChar_x - 15, gameChar_y - 19, 30, 7);
+		//Legs
+		fill(0, 0, 255);
+		rect(gameChar_x - 5, gameChar_y - 12, 20, 15);
+		rect(gameChar_x - 25, gameChar_y - 12, 20, 9);
+		//Arms
+		fill(200, 100, 100);
+		rect(gameChar_x - 7, gameChar_y - 46, 15, 37);
 
 	}
 	else if(isRight)
 	{
 		// add your walking right code
+		 //Head
+		 fill(255, 227, 48);
+		 rect(gameChar_x - 15, gameChar_y - 72, 30, 26, 5);
+		 rect(gameChar_x - 6, gameChar_y - 78, 12, 10);
+		 //Eyes and mouth
+		 stroke(60);
+		 strokeWeight(3);
+		 point(gameChar_x + 6, gameChar_y - 64);
+		 strokeWeight(2);
+		 line(gameChar_x + 10, gameChar_y - 54, gameChar_x +15, gameChar_y - 54);
+		 //body
+		 strokeWeight(1);
+		 fill(0);
+		 rect(gameChar_x - 15, gameChar_y - 46, 30, 27);
+		 //Belt
+		 fill(98, 74, 46);
+		 rect(gameChar_x - 15, gameChar_y - 19, 30, 7);
+		 //Legs
+		 fill(0, 0, 255);
+		 rect(gameChar_x - 15, gameChar_y - 12, 20, 15);
+		 rect(gameChar_x + 5, gameChar_y - 12, 20, 9);
+		 //Arms
+		 fill(200, 100, 100);
+		 rect(gameChar_x - 7, gameChar_y - 46, 15, 37);
 
 	}
 	else if(isFalling || isPlummeting)
 	{
 		// add your jumping facing forwards code
-
+		fill(255, 227, 48);
+		rect(gameChar_x - 15, gameChar_y - 72, 30, 26, 5);
+		rect(gameChar_x - 6, gameChar_y - 78, 12, 10);
+		//Eyes and mouth
+		stroke(60);
+		strokeWeight(3);
+		point(gameChar_x - 6, gameChar_y - 64);
+		point(gameChar_x + 6, gameChar_y - 64);
+		strokeWeight(2);
+		line(gameChar_x - 3, gameChar_y - 54, gameChar_x + 3, gameChar_y - 54);
+		//Body
+		strokeWeight(1);
+		fill(0);
+		rect(gameChar_x - 18, gameChar_y - 46, 37, 27);
+		//Arms
+		fill(200, 100, 100);
+		rect(gameChar_x - 25, gameChar_y - 46, 7, 37);
+		rect(gameChar_x + 18, gameChar_y - 46, 7, 37);
+		//Belt
+		fill(98, 74, 46);
+		rect(gameChar_x - 18, gameChar_y - 19, 36, 7);
+		//Legs
+		fill(0, 0, 255);
+		rect(gameChar_x - 18, gameChar_y - 12, 17, 3);
+		rect(gameChar_x + 1, gameChar_y - 12, 17, 3);
 	}
 	else
 	{
 		// add your standing front facing code
     //Head
-    fill(255, 227, 48); 
-    rect(gameChar_x - 15, gameChar_y - 72, 30, 26, 5);
-    rect(gameChar_x - 6, gameChar_y - 78, 12, 10);
-    //Eyes and mouth
-    stroke(60);
-    strokeWeight(3);
-    point(gameChar_x - 6, gameChar_y - 64);
-    point(gameChar_x + 6, gameChar_y - 64);
-    strokeWeight(2);
-    line(gameChar_x - 3, gameChar_y - 54, gameChar_x + 3, gameChar_y - 54);
-    //Body
-    strokeWeight(1);
-    fill(0);
-    rect(gameChar_x - 18, gameChar_y - 46, 37, 27);
-    //Arms
-    fill(200, 100, 100);
-    rect(gameChar_x - 25, gameChar_y - 46, 7 , 37);
-    rect(gameChar_x + 18, gameChar_y - 46, 7 , 37);
-    //Belt
-    fill(98, 74, 46);
-    rect(gameChar_x - 18, gameChar_y - 19, 36, 7);
-    //Legs
-    fill(0, 0, 255);
-    rect(gameChar_x - 18, gameChar_y - 12, 17, 15);
-    rect(gameChar_x + 1, gameChar_y - 12, 17, 15);
+		fill(255, 227, 48); 
+		rect(gameChar_x - 15, gameChar_y - 72, 30, 26, 5);
+		rect(gameChar_x - 6, gameChar_y - 78, 12, 10);
+		//Eyes and mouth
+		stroke(60);
+		strokeWeight(3);
+		point(gameChar_x - 6, gameChar_y - 64);
+		point(gameChar_x + 6, gameChar_y - 64);
+		strokeWeight(2);
+		line(gameChar_x - 3, gameChar_y - 54, gameChar_x + 3, gameChar_y - 54);
+		//Body
+		strokeWeight(1);
+		fill(0);
+		rect(gameChar_x - 18, gameChar_y - 46, 37, 27);
+		//Arms
+		fill(200, 100, 100);
+		rect(gameChar_x - 25, gameChar_y - 46, 7 , 37);
+		rect(gameChar_x + 18, gameChar_y - 46, 7 , 37);
+		//Belt
+		fill(98, 74, 46);
+		rect(gameChar_x - 18, gameChar_y - 19, 36, 7);
+		//Legs
+		fill(0, 0, 255);
+		rect(gameChar_x - 18, gameChar_y - 12, 17, 15);
+		rect(gameChar_x + 1, gameChar_y - 12, 17, 15);
 
 	}
 
@@ -119,7 +238,8 @@ function keyPressed()
 	console.log("keyPressed: " + keyCode);
 
 	if (keyCode == 37)
-	{	anonymous
+	{	
+		isLeft = true;
 		console.log("isLeft value " + isLeft);
 	}
 	else if (keyCode == 39)
